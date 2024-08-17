1512d34d2605287683da5ef3f326cc4bcab711a4
Ryacuda <adam.germain1@gmail.com>
2024-08-17 16:39:19 +0200

    proj collision effects

--- Assets/Scripts/CharacterTargetBehaviour.cs
+++ Assets/Scripts/CharacterTargetBehaviour.cs
@@ -8,10 +8,14 @@ public class CharacterTargetBehaviour : MonoBehaviour
 	public int statStr;
 	public int statCrea;
 
+    Transform spriteTransform;
+    static float FXDuration = 0.5f;
+    static float scaleFactor = 0.15f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+		spriteTransform = transform.Find("Sprite");
     }
 
     // Update is called once per frame
@@ -22,19 +26,49 @@ public class CharacterTargetBehaviour : MonoBehaviour
 
     public void IncrementStat(ChromosomeType chromType)
     {
+        Color color;
+
         switch(chromType)
         {
             case ChromosomeType.Intelligence:
-                statInt++; break;
+                statInt++;
+                color = Color.blue;
+                break;
 
             case ChromosomeType.Creativity:
+                color = Color.green;
                 statCrea++; break;
 
             case ChromosomeType.Strength:
+                color = Color.red;
                 statStr++; break;
 
             default:
+                color = Color.white;
                 break;
         }
+
+        StartCoroutine(StatIncreaseFX(color));
+    }
+
+    private IEnumerator StatIncreaseFX(Color color)
+    {
+        float elapsed_time = 0.0f;
+        Vector3 intial_scale = spriteTransform.localScale;
+        // bouncy
+        while (elapsed_time < FXDuration)
+        {
+            float x = elapsed_time * Mathf.PI * 3 / FXDuration;
+            Debug.Log(x);
+            spriteTransform.localScale = ( 1 + (scaleFactor * (2 * Mathf.Sin(x / 3) + Mathf.Sin(x)))) * Vector3.one;
+			elapsed_time += Time.deltaTime;
+
+			yield return null;
+        }
+
+        // reset scale to starting scale
+        spriteTransform.localScale = intial_scale;
+
+        yield return null;
     }
 }
